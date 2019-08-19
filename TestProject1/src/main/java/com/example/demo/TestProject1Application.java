60326c88543802951f312e24fee185df553742ee
shruthisujith <shruthi.sujit@somaiya.edu>
2019-08-19 14:39:02 +0530

    adding files for the first time

---
+++ TestProject1/src/main/java/com/example/demo/TestProject1Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example.*")
+
+public class TestProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProject1Application.class, args);
+	}
+
+}
